b5935b99e1519fb92d49556dd24f9eeaa56bc573
diego <diego@192.168.5.199>
2021-10-17 10:25:44 +0200

    patientForm GUI

--- src/Pojos/Telemedicine.java
+++ src/Pojos/Telemedicine.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package telemedicine;
+package Pojos;
 
 /**
  *
  * @author macbookair
  */
-class Doctor {
+public class Telemedicine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
